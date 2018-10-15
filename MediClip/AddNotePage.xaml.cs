6409712a176336e6d88cd150f7406b4a8f4b6daa
Francois-vv <42727352+Francois-vv@users.noreply.github.com>
2018-10-15 21:00:38 +1100

    got rid of redundant page "takephoto"

--- MediClip/AddNotePage.xaml.cs
+++ MediClip/AddNotePage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,16 +19,50 @@ namespace MediClip
 		public AddNotePage ()
 		{
 			InitializeComponent ();
-		}
+
+            CameraButton.Clicked += CameraButton_Clicked;
+        }
 
         private void Handle_Activated(object sender, System.EventArgs e)
         {
             Navigation.PushAsync(new MenuPage());
         }
 
-        private void AddPhoto_Clicked(object sender, System.EventArgs e)
+        private async void CameraButton_Clicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new AddPhotoPage());
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var cameraStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Camera);
+            var storageStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Storage);
+
+            if (cameraStatus != PermissionStatus.Granted || storageStatus != PermissionStatus.Granted)
+            {
+                var results = await CrossPermissions.Current.RequestPermissionsAsync(new[] { Permission.Camera, Permission.Storage });
+                cameraStatus = results[Permission.Camera];
+                storageStatus = results[Permission.Storage];
+            }
+
+            if (cameraStatus == PermissionStatus.Granted && storageStatus == PermissionStatus.Granted)
+            {
+                var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    SaveToAlbum = true,
+                    //Directory = "Sample",
+                    //Name = "test.jpg"
+                });
+            }
+            else
+            {
+                await DisplayAlert("Permissions Denied", "Unable to take photos.", "OK");
+                //On iOS you may want to send your user to the settings screen.
+                CrossPermissions.Current.OpenAppSettings();
+            }
         }
 
         private void Submit_Clicked(object sender, System.EventArgs e)
