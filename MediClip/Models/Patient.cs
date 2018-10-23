7ac450af10d57c91d0723975cd741f9383dd56c6
Francois-vv <42727352+Francois-vv@users.noreply.github.com>
2018-10-23 16:39:46 +1100

    Made the Patient FullName display

--- MediClip/Models/Patient.cs
+++ MediClip/Models/Patient.cs
@@ -6,15 +6,53 @@ namespace MediClip.Models
 {
     public class Patient
     {
+        private String myFirstName;
+        private String myLastName;
+
         public int PatientID { get; set; }
         public int WardID { get; set; }
         public String AssignDateFrom { get; set; }
         public String AssignDateTo { get; set; }
-        public String FirstName { get; set; }
-        public String LastName { get; set; }
+        public String FirstName
+        {
+            get
+            {
+                return myFirstName;
+            }
+
+            set
+            {
+                myFirstName = value;
+            }
+        }
+        public String LastName
+        {
+            get
+            {
+                return myLastName;
+            }
+
+            set
+            {
+                myLastName = value;
+            }
+        }
         public String Dob { get; set; }
         public String Sex { get; set; }
         public String Dosage { get; set; }
         public String Picture { get; set; }
+
+        
+        public String FullName
+        {
+
+            get
+            {
+                return myFirstName + " " + myLastName;
+            }
+
+        }
+
+        
     }
 }
