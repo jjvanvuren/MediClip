fd4af658c91d8d42581515453406e0826fec3ff1
jjvanvuren <c3252194@uon.edu.au>
2018-10-24 18:10:09 +1100

    Added NotePage

---
+++ MediClip/NotePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MediClip
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NotePage : ContentPage
+	{
+		public NotePage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Handle_Activated(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new MenuPage());
+        }
+    }
+}
